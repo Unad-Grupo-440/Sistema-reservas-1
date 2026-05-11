c3d018beab7a5d20962b0f8edf76451a63f6c1c6
Brimafer <brimafervp@gmail.com>
2026-05-11 12:15:47 -0500

    Update main.py
    Ajuste codigo

--- main.py
+++ main.py
@@ -3,60 +3,56 @@ from servicio import *
 from excepcion import *
 import logging
 
+
 def pruebas():
 
-    print("===== INICIO DE PRUEBAS =====\n")
+    print("==== INICIO PRUEBAS ====\n")
 
-    # 1. Reserva válida
+    # 1
     try:
-        r1 = ReservaSala(2, "vip")
-        print(r1.descripcion())
-        print("Costo:", r1.calcular_costo())
+        s = ReservaSala(2, "vip")
+        print(s.descripcion())
+        print(s.calcular_costo())
     except Exception as e:
         logging.error(e)
-        print("Error:", e)
 
-    # 2. Reserva inválida
+    # 2
     try:
-        r2 = ReservaSala(-1, "vip")
+        ReservaSala(-1, "vip")
     except Exception as e:
         logging.error(e)
-        print("Error:", e)
 
-    # 3. Equipo válido
+    # 3
     try:
-        eq1 = AlquilerEquipo("video beam", 3)
-        print(eq1.descripcion())
-        print("Costo:", eq1.calcular_costo())
+        e = AlquilerEquipo("video beam", 2)
+        print(e.descripcion())
+        print(e.calcular_costo())
     except Exception as e:
         logging.error(e)
-        print("Error:", e)
 
-    # 4. Equipo inválido
+    # 4
     try:
-        eq2 = AlquilerEquipo("equipo falso", 3)
+        AlquilerEquipo("fake", 2)
     except Exception as e:
         logging.error(e)
-        print("Error:", e)
 
-    # 5. Asesoría válida
+    # 5
     try:
-        a1 = Asesoria("intermedio", 2)
-        print(a1.descripcion())
-        print("Costo:", a1.calcular_costo())
+        a = Asesoria("intermedio", 3)
+        print(a.descripcion())
+        print(a.calcular_costo())
     except Exception as e:
         logging.error(e)
-        print("Error:", e)
 
-    # 6. Asesoría inválida
+    # 6
     try:
-        a2 = Asesoria("nivel falso", 2)
+        Asesoria("malo", 3)
     except Exception as e:
         logging.error(e)
-        print("Error:", e)
 
-    print("\n===== FIN DE PRUEBAS =====")
+    print("\n==== FIN PRUEBAS ====")
 
 
 if __name__ == "__main__":
     pruebas()
+
